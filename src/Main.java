a528c0b26af4f4c4a1449cd3e6efa680e502bab9
Alexey Lyalyakin <varan560045@yandex.ru>
2024-03-23 13:43:30 +0300

    Создал SomeClass Написал строку кода

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Создал ветку dev");
         System.out.println("Создал ветку feature1");
+        System.out.println("Написал какой-то код");
     }
 }
